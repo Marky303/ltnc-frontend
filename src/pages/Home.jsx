1342acc5df43640525898999a4992eea8c44b882
vdhoan <156833044+vdhoan@users.noreply.github.com>
2024-04-23 18:48:17 +0700

    Delete src/pages directory

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Navigate } from "react-router-dom";
-
-const Home = () => {
-  return <div>This is the home page!</div>;
-};
-
-export default Home;
